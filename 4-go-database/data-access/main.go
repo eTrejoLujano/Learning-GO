4181db5f1d0b3e4e3ebe831fa2d2e93546989dd9
Erik Trejo Lujano <trejolujano.erik@gmail.com>
2023-12-03 17:50:44 -0800

    error with psql, switching to mysql next

--- 4-go-database/data-access/main.go
+++ 4-go-database/data-access/main.go
@@ -3,59 +3,67 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/lib/pq"
 )
+
 const (
-  host     = "localhost"
-  port     = 5432
-  user     = "eriktrejolujano"
-  password = ""
-  dbname   = "recordings"
+	host     = "localhost"
+	port     = 5432
+	user     = "eriktrejolujano"
+	password = ""
+	dbname   = "recordings"
 )
 
 type Album struct {
-    ID     int64
-    Title  string
-    Artist string
-    Price  float32
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
 }
-  var psqlInfo = fmt.Sprintf("host=%s port=%d user=%s "+
-    "password=%s dbname=%s sslmode=disable",
-    host, port, user, password, dbname)
-	var db, err = sql.Open("postgres", psqlInfo)
+
+var psqlInfo = fmt.Sprintf("host=%s port=%d user=%s "+
+	"password=%s dbname=%s sslmode=disable",
+	host, port, user, password, dbname)
+var db, err = sql.Open("postgres", psqlInfo)
 
 func main() {
-  if err != nil {
-    panic(err)
-  }
-  defer db.Close()
-  err = db.Ping()
-  if err != nil {
-    panic(err)
-  }
-  fmt.Println("Established a successful connection!")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
+	fmt.Println("Established a successful connection!")
 }
 
 // albumsByArtist queries for albums that have the specified artist name.
 func albumsByArtist(name string) ([]Album, error) {
-    // An albums slice to hold data from returned rows.
-    var albums []Album
-    rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
-    if err != nil {
-        return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-    }
-    defer rows.Close()
-    // Loop through rows, using Scan to assign column data to struct fields.
-    for rows.Next() {
-        var alb Album
-        if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
-            return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-        }
-        albums = append(albums, alb)
-    }
-    if err := rows.Err(); err != nil {
-        return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-    }
-    return albums, nil
+	// An albums slice to hold data from returned rows.
+	var albums []Album
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+	// Loop through rows, using Scan to assign column data to struct fields.
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	return albums, nil
 }
